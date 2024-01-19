f594438773e3b937ba663a19d8bfc90aef402e9b
Armando Yanez <ayanez20@wou.edu>
2024-01-18 21:49:51 -0800

    Inital commit with working carasoul

---
+++ PaintingSite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PaintingSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
